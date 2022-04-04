59b942aa236a77c0770f1eacecec9aee43f14cc3
samtuhka <samuel.tuhkanen@helsinki.fi>
2022-04-04 15:53:20 +0300

    Update read.py

--- read.py
+++ read.py
@@ -1,13 +1,22 @@
-from pyodide import open_url
+from js import fetch
+from matplotlib import image
 
-#change filename to the filename in the exercise, fetches it from online
+#change the filenmame to the one in the relevant exercise
 filename = "hy_admission_stats_2017.csv"
 url= "https://raw.githubusercontent.com/samtuhka/SciProgFiles/main/" + filename
-content = open_url(url).read()
+
+res = await fetch(url)
+text = await res.text()
 
 with open(filename, 'w') as f:
-    f.write(content)
+    f.write(text)
 
-# now we can start coding normally
-file = open("hy_admission_stats_2017.csv")
-print(file.read())
+#ugly hack for images used in exercise set 4
+if "png" in filename:
+    img = np.loadtxt(filename)
+    image.imsave(filename, img,  cmap='gray')
+
+# NORMAL CODE STARTS HERE
+# Change lines below to your own code
+f = open(filename)
+print(f.read())
